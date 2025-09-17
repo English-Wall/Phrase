098c85eecddb048055811b4821ea09b534d68e85
English-Wall <rup820326@gmail.com>
2025-09-17 16:03:05 +0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-function checkAnswer(button, result) {
-  const feedback = button.parentElement.querySelector('.feedback');
-  const example = button.parentElement.querySelector('.example');
-  feedback.innerText = result === 'O' ? '✔️ 正確' : '❌ 錯誤';
-  example.style.display = 'block';
-}
